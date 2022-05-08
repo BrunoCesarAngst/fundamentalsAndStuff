4bf411ce1343b8814a95b46458113475908ff9df
brunocesarangst <bruno.angst@rede.ulbra.br>
2022-05-08 16:45:33 -0300

    initial

---
+++ basicCrudApp.js
@@ -0,0 +1,18 @@
+const app = {
+  anyUsers: [
+    {
+      id: 1,
+      name: 'Jonh Doe',
+    },
+    // ...outros usuários
+  ],
+  anyContents: [
+    {
+      id: 1,
+      userId: 1,
+      content: 'Lorem ipsum dolor...',
+    },
+    // ...outras conteúdos
+  ],
+  // ... outros identificadores
+}
